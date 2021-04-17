4039960a79ec9e0e25d1b6a3dea6f3ab810c2241
cloudbreathe <manjunathpv@cloudbreathe.in>
2021-04-17 14:03:41 +0530

    Add Forwarded headers

--- CoreMusic/Startup.cs
+++ CoreMusic/Startup.cs
@@ -33,6 +33,11 @@ namespace CoreMusic
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
